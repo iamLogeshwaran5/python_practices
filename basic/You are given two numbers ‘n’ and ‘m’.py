f832366895e81baeb42b1076cf1aee44f10b794e
Logeshwaran <152408417+iamLogeshwaran5@users.noreply.github.com>
2023-12-15 07:17:36 +0530

    Add files via upload

---
+++ basic/You are given two numbers ‘n’ and ‘m’.py
@@ -0,0 +1,23 @@
+"""
+You are given two numbers. Your task is to multiply the two numbers and print the answer.
+
+Input Description:
+You are given two numbers ‘n’ and ‘m’.
+
+Output Description:
+Print the multiplied answer
+
+Sample Input :
+99999 99998
+Sample Output :
+9999700002
+"""
+
+# Get input
+n, m = map(int, input().split())
+
+# Multiply the numbers
+result = n * m
+
+# Print the multiplied answer
+print(result)
