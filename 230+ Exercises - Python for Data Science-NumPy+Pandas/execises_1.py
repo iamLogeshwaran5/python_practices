d86046547b2a03c60bb2351eae9033aee2ee9824
Logeshwaran <152408417+iamLogeshwaran5@users.noreply.github.com>
2024-04-01 20:44:04 +0530

    Update_Alternative_Solution_execises_1.py

--- 230+ Exercises - Python for Data Science-NumPy+Pandas/execises_1.py
+++ 230+ Exercises - Python for Data Science-NumPy+Pandas/execises_1.py
@@ -37,3 +37,28 @@ print("A:", np.all(A))
 print("B:", np.all(B))
 print("C:", np.all(C))
 print("D:", np.all(D))
+
+"""
+Another way  solution 
+"""
+
+import numpy as np
+ 
+ 
+A = np.array([[3, 2, 1, 4],
+              [5, 2, 1, 6]])
+ 
+B = np.array([[3, 2, 1, 4],
+              [5, 2, 0, 6]])
+ 
+C = np.array([[True, False, False],
+              [True, True, True]])
+ 
+D = np.array([0.1, 0.3])
+ 
+for name, array in zip(list('ABCD'), [A, B, C, D]):
+    print(f'{name}: {np.all(array)}')
+
+
+
+
